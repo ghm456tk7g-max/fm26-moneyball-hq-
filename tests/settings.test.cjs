eab0c7d6e684199abfe21813db9b9db6a7eee20e
ghm456tk7g-max <ghm456tk7g@privaterelay.appleid.com>
2026-09-03 19:07:29 +0200

    test: align default formation with 4-3-2-1

--- tests/settings.test.cjs
+++ tests/settings.test.cjs
@@ -3,15 +3,15 @@ const assert = require('node:assert/strict');
 const { DEFAULT_SETTINGS, normalizeSettings } = require('../electron/settings.cjs');
 
 test('normalizes persisted numeric settings', () => {
-  assert.deepEqual(normalizeSettings({ transferBudget: '65000', maxWeeklyWage: '1000', formation: ' 4-2-3-1 ' }), DEFAULT_SETTINGS);
+  assert.deepEqual(normalizeSettings({ transferBudget: '65000', maxWeeklyWage: '1000', formation: ' 4-3-2-1 ' }), DEFAULT_SETTINGS);
   assert.deepEqual(normalizeSettings({ transferBudget: 0, maxWeeklyWage: 0, formation: '4-3-2-1' }), { transferBudget: 0, maxWeeklyWage: 0, formation: '4-3-2-1' });
 });
 
 test('rejects negative, non-finite, excessive and malformed settings', () => {
-  assert.throws(() => normalizeSettings({ transferBudget: -1, maxWeeklyWage: 1, formation: '4-2-3-1' }), /Transferbudget/);
-  assert.throws(() => normalizeSettings({ transferBudget: 'no', maxWeeklyWage: 1, formation: '4-2-3-1' }), /Transferbudget/);
-  assert.throws(() => normalizeSettings({ transferBudget: '', maxWeeklyWage: 1, formation: '4-2-3-1' }), /Transferbudget/);
-  assert.throws(() => normalizeSettings({ transferBudget: 1, maxWeeklyWage: Number.POSITIVE_INFINITY, formation: '4-2-3-1' }), /Wochengehalt/);
+  assert.throws(() => normalizeSettings({ transferBudget: -1, maxWeeklyWage: 1, formation: '4-3-2-1' }), /Transferbudget/);
+  assert.throws(() => normalizeSettings({ transferBudget: 'no', maxWeeklyWage: 1, formation: '4-3-2-1' }), /Transferbudget/);
+  assert.throws(() => normalizeSettings({ transferBudget: '', maxWeeklyWage: 1, formation: '4-3-2-1' }), /Transferbudget/);
+  assert.throws(() => normalizeSettings({ transferBudget: 1, maxWeeklyWage: Number.POSITIVE_INFINITY, formation: '4-3-2-1' }), /Wochengehalt/);
   assert.throws(() => normalizeSettings({ transferBudget: 1, maxWeeklyWage: 1, formation: '' }), /Formation/);
   assert.throws(() => normalizeSettings({ transferBudget: 1, maxWeeklyWage: 1, formation: 'x'.repeat(41) }), /Formation/);
 });
